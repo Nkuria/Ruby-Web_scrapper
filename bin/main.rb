ce41eaa6e79e97d712fd8574eaaa30f9c211a1ee
Nkuria <nkuria01@gmail.com>
2020-11-15 00:26:52 +0300

    Created the store method in scrapper class and worked on it

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,4 @@
 #!/usr/bin/env ruby
 require_relative '../lib/scrapper.rb'
-
+scrap = Scrapper.new
+scrap.store
--- lib/scrapper.rb
+++ lib/scrapper.rb
@@ -2,11 +2,27 @@ require 'HTTParty'
 require 'Nokogiri'
 require 'JSON'
 require 'Pry'
+require 'byebug'
 class Scrapper
   def parser
     link = 'https://www.kayak.com/Zanzibar-Hotels.1557.hotel.ksp?a=adwordssearch&encoder=5_1&enc_aid=93646239998&enc_tid=dsa-866536695082&enc_locp=9076864&enc_loci=&enc_mt=b&enc_n=g&enc_d=c&enc_cid=414735300154&enc_pos=&gclid=Cj0KCQiAnb79BRDgARIsAOVbhRptNtM-fhda6l2GDkHhhqZPooUxtig6EAEHSeVi5MLCj1qwmlOAY7saAm33EALw_wcB'
     raw_page = HTTParty.get(link)
     parsed_page = Nokogiri::HTML(raw_page)
   end
-  
+  def store
+    all_hotels = Array.new
+    hotels = parser.css('div.slickHotelCardReworked')
+    first_hotel = hotels.first
+    hotels.each do |hotel|
+      each_hotel = {
+        name: hotel.css('a.hotelCard__title').text,
+        price: hotel.css('span.hotelCard__price').text,
+        amenities: hotel.css('span.freebie__text').text,
+        booking_link: "https://www.kayak.com" + hotel.css('div.hotelCard__informationWrapper__leftSide').css('a')[0].attributes["href"].value
+      }
+      all_hotels << each_hotel 
+       
+    end
+    byebug
+  end
 end
