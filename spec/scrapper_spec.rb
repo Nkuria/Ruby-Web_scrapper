dd3ec5983293eb8da0daab18bad67460ba1ba329
Nkuria <nkuria01@gmail.com>
2020-11-15 10:45:23 +0300

    Run rubocop to fix errors

--- spec/scrapper_spec.rb
+++ spec/scrapper_spec.rb
@@ -12,11 +12,11 @@ describe Scrapper do
       expect(scrape1.parser.is_a?(Object)).to eql(true)
     end
     it 'returns an object type' do
-        expect(scrape1.parser.is_a?(String)).to eql(false)
-      end
-      it 'returns an object type' do
-        expect(scrape1.parser.is_a?(Integer)).to eql(false)
-      end
+      expect(scrape1.parser.is_a?(String)).to eql(false)
+    end
+    it 'returns an object type' do
+      expect(scrape1.parser.is_a?(Integer)).to eql(false)
+    end
     it 'returns an object containing hotels which should not be empty' do
       expect(scrape1.parser.nil?).to eql(false)
     end
