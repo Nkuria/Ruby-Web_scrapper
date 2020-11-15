612e8b4214e95682c08a12437a2fdf607f3640d3
Nkuria <nkuria01@gmail.com>
2020-11-15 03:53:50 +0300

    Added requre spec_helper

--- spec/scrapper_spec.rb
+++ spec/scrapper_spec.rb
@@ -1,12 +1,13 @@
 require_relative '../lib/scrapper.rb'
+require_relative '../spec/spec_helper.rb'
 
 describe Scrapper do
   let(:scrape1) { Scrapper.new }
 
   describe '#parser' do
-    # it 'returns the parsed version of the page' do
-    #   expect(scrape1.parser.respond_to?(:to_s)).to eql(true)
-    # end
+    it 'returns the parsed version of the page' do
+      expect(scrape1.parser.respond_to?(:to_s)).to eql(true)
+    end
     it 'returns an object type' do
       expect(scrape1.parser.is_a?(Object)).to eql(true)
     end
