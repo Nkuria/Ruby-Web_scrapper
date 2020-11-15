6e679351d0d68a2de656cfe5e112ea1a4fd92b71
Nkuria <nkuria01@gmail.com>
2020-11-15 04:42:46 +0300

    Run rubocop

--- lib/scrapper.rb
+++ lib/scrapper.rb
@@ -1,5 +1,5 @@
-require 'nokogiri'
 require 'httparty'
+require 'nokogiri'
 class Scrapper
   def parser
     link = 'https://www.kayak.com/Zanzibar-Hotels.1557.hotel.ksp?a=adwordssearch&encoder=5_1&enc_aid=93646239998&enc_tid=dsa-866536695082&enc_locp=9076864&enc_loci=&enc_mt=b&enc_n=g&enc_d=c&enc_cid=414735300154&enc_pos=&gclid=Cj0KCQiAnb79BRDgARIsAOVbhRptNtM-fhda6l2GDkHhhqZPooUxtig6EAEHSeVi5MLCj1qwmlOAY7saAm33EALw_wcB'
