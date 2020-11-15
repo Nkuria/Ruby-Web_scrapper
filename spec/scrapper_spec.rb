4dca54f31b6dfd872746f4e2faacce938e5d6f4c
Nkuria <nkuria01@gmail.com>
2020-11-15 10:44:46 +0300

    Added more tests to check for both negative and positive scenarios

--- spec/scrapper_spec.rb
+++ spec/scrapper_spec.rb
@@ -11,9 +11,21 @@ describe Scrapper do
     it 'returns an object type' do
       expect(scrape1.parser.is_a?(Object)).to eql(true)
     end
+    it 'returns an object type' do
+        expect(scrape1.parser.is_a?(String)).to eql(false)
+      end
+      it 'returns an object type' do
+        expect(scrape1.parser.is_a?(Integer)).to eql(false)
+      end
+    it 'returns an object containing hotels which should not be empty' do
+      expect(scrape1.parser.nil?).to eql(false)
+    end
   end
 
   describe '#store' do
+    it 'returns an array of hotels which should be greater than 0' do
+      expect(scrape1.store.empty?).to eql(false)
+    end
     it 'returns an array of hotels and should be greater than 0' do
       expect(scrape1.store.nil?).to eql(false)
     end
@@ -22,6 +34,9 @@ describe Scrapper do
     end
     it 'returns an array of hotels' do
       expect(scrape1.store.is_a?(String)).to eql(false)
+    end
+    it 'returns an array of hotels' do
+      expect(scrape1.store.is_a?(Integer)).to eql(false)
     end
   end
 end
